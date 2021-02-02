5c48024e6488282caf2e352821e89325e337dda7
Yutai Zhou <yutai.zhou@ll.mit.edu>
2021-02-01 21:39:27 -0500

    started run and readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# drlnd_p3_collab-compet
+Project 3 of the Udacity Deep Reinforcement Learning Nanodegree Program
+
+**Project Details**
+
+The envionrment is Tennis from Unity ML Agents. It is a multi-agent environment where two agents, each controlling a tennis racket from opposite sides of a tennis net, must keep the ball bouncing over the net and in play.
+
+State Space: Continuous. 8 element vector that corresponds to the position and the velocity of the ball and racket. The environment does further preprocessing for stacking observation from three frames into one vector, thus forming a 3x8=24 element vector. Each agent gets its own local observation. 
+
+Action Space: Contiuous. 2 element vector that corresponds horizontal movement relative to the net, and jumping.
+
+Reward: An agent gains +0.1 reward if it hits the ball over the net, but loses -0.01 if it lets a ball reach the ground or hit the ball out of bounds. 
+
+Episodic task, where each episode is 1000 steps through the environment. 
+
+The task is considered solved when the meta-agent controlling both players gets an average score of +0.5 over 100 episodes. The score of an episode for a meta agent is taken as the maximum over the scores individually obtained by the two agents it controls.
+
+Reward Structure:
+In this kind of multi-agent environments, the kind of reward signals provided by the environment can dictate whether the agents' optimal behavior that maximizes the rewards is cooperative, competitive, or mixed. In this case, where +0.1 is awarded for hitting the ball over a net, and only -0.01 is dealt as punishment for letting the ball hit the ground or go out of bounds, the agents will have much higher incentive to learn to keep the ball volleying back and forth.
+
+
+**Repository Layout**
+- results/: the latest run from the implemented algorithms, containing the score numpy file, plot of score, agent model, and progress log. 
+- src/: source code for the agent, which is separated into code specific to agent itself, reacher environment, and utilities
+- Continuous_Control.ipynb: this is simply a file copied over from the official repository. Safe to ignore.
+- run.py: code run running, evaluating, and logging the agent. 
+
+**Getting Started** 
+
+Create a conda environment from the environment.yml file:
+conda env create -f environment.yml
+
+Activate the newly created environment:
+conda activate drlnd
+
+**Instructions**
+
+At the time of writing (Jan 31, 2021), 1 algorithm has been implemented: DDPG.
+
+To run DDPG without loading any weights, run the following:
+```
+python run.py DDPG
+```
+Note: unless the directory path has been changed in run.py, this will overwrite the previous results.
+
---
+++ run.py
@@ -0,0 +1,92 @@
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import trange
+from unityagents import UnityEnvironment
+
+from src.agents.agent import DDPG
+from src.utils.utils import Logger
+from src.utils.typing import List
+
+
+def rollout(agent, env: UnityEnvironment, is_training: bool = True):
+    # completes one episode of rollout, is_training functionality not really fully fledged out
+    env_info = env.reset(train_mode=True)[brain_name] 
+    
+    states = env_info.vector_observations
+    total_reward = 0
+    ended = False
+    while not ended:
+        actions = agent.act(states, is_training)
+
+        env_info = env.step(actions)[brain_name]
+        next_states, rewards, dones = env_info.vector_observations, np.array(env_info.rewards), np.array(env_info.local_done)
+        ended = False not in dones
+        agent.step(states, actions, rewards, next_states, dones)
+        states = next_states
+        total_reward += rewards
+
+    return total_reward.max()
+
+
+def run(agent, agent_name, env: UnityEnvironment, num_episodes=10000, is_training=True) -> List[float]:
+    scores = []
+    max_avg_score = -np.inf
+    solved = False
+
+    logger = Logger(f'results/{agent_name}/progress.txt')
+    logger.write(f'Progress for {agent_name} agent\n')
+    for i_episode in trange(1, num_episodes+1):
+        total_reward = rollout(agent, env, is_training)
+        scores.append(total_reward)
+
+        if is_training:
+            if len(scores) >= 100:
+                avg_score = np.mean(scores[-100:])
+                max_avg_score = max(max_avg_score, avg_score)
+            
+            if i_episode % 100 == 0:
+                logger.write(f'Episode {i_episode}/{num_episodes} | Max Average Score: {max_avg_score}\n')
+            if max_avg_score >= 30 and not solved:
+                logger.write(f'Task solved in {i_episode} episodes, with average score over the last 100 episode: {max_avg_score}\n')
+                solved = True
+
+    logger.close()
+    agent.save(f'results/{agent_name}/checkpoint.pth')
+    with open(f'results/{agent_name}/scores.npy', 'wb') as f:
+        np.save(f, scores)
+    return scores
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='This is a runner for DRLND Project 3: Collaboration and Competition')
+    parser.add_argument('algorithm', type=str)
+    args = parser.parse_args()
+
+    # set up env
+    env = UnityEnvironment(file_name="src/envs/Tennis_Linux_NoVis/Tennis.x86_64")
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+    env_info = env.reset(train_mode=True)[brain_name]
+
+
+    # set up agent
+    action_size = brain.vector_action_space_size
+    state_size = len(env_info.vector_observations[0])
+    num_agents = len(env_info.agents)
+
+    algorithms = {'MADDPG': MADDPG}
+    algorithm = algorithms[args.algorithm]
+    agent = algorithm(state_size, action_size, num_agents)
+
+    scores = run(agent, args.algorithm, env, num_episodes=2000)
+
+    # plot the scores
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(start=1, stop=len(scores)+1), scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    fig.savefig(f'results/{args.algorithm}/result.png')
+    plt.close(fig)
+

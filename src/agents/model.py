27fe1dc0af1c50c96dbde05e19c0278a4bb17fa0
Yutai Zhou <yutai.zhou@ll.mit.edu>
2021-02-02 08:12:09 -0500

    centralized model

---
+++ src/agents/model.py
@@ -0,0 +1,56 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
+
+class Actor(nn.Module):
+    def __init__(self, state_size, action_size, hidden_sizes=[256,256], seed=0):
+        super().__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, hidden_sizes[0])
+        self.fc2 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
+        self.fc3 = nn.Linear(hidden_sizes[1], action_size)
+        self.init_params()
+
+    def init_params(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = torch.tanh(self.fc3(x))
+        return x
+
+class Critic(nn.Module):
+    def __init__(self, full_state_size, hidden_sizes = [256, 256], seed=0):
+        super().__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(full_state_size, hidden_sizes[0])
+        self.fc2 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
+        self.fc3 = nn.Linear(hidden_sizes[1], 1)
+        self.init_params()
+    
+    def init_params(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+    
+    def forward(self, states, actions):
+        """Q-Value critic"""
+        full_state = self._centralize(states, actions)
+        x = F.relu(self.fc1(full_state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    @staticmethod
+    def _centralize(state, actions):
+
+        return None
